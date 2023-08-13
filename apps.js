edcbc0bd18cd0046193d5564e2c99ba3d098a0e4
tonmoy <tonmoy3673@gmail.com>
2023-08-13 22:32:16 +0600

    display phone image

--- apps.js
+++ apps.js
@@ -14,7 +14,29 @@ const displayPhone=async()=>{
   const url='https://openapi.programming-hero.com/api/phones?search=iphone'
     const res=await fetch(url)
     const data=await res.json();
-    console.log(data.data)
+    showPhones(data.data)
 };
+
+const showPhones=(phones)=>{
+    const phoneDiv=document.getElementById('phone-container')
+    phones.forEach(phone => {
+        const {image,phone_name,brand,slug}=phone;
+    const oneDiv=document.createElement('div');
+    oneDiv.innerHTML=`
+    <div class="col">
+    <div class="card">
+      <img src=${image} class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+    
+    `
+    phoneDiv.appendChild(oneDiv)
+    });
+}
+
 
 displayPhone();
