0743b90b683e3cd1b1ebc4971271551989880511
tonmoy <tonmoy3673@gmail.com>
2023-09-10 23:34:46 +0600

    added card

--- apps.js
+++ apps.js
@@ -13,6 +13,16 @@ const loadPhone=async()=>{
     const url='https://openapi.programming-hero.com/api/phones?search=iphone';
     const res= await fetch(url);
     const data=await res.json();
-    console.log(data.data);
+    displayPhone(data.data);
 };
+
+const displayPhone=(phones)=>{
+    
+    phones.forEach(phone => {
+        
+    });
+
+
+}
+
 loadPhone();
