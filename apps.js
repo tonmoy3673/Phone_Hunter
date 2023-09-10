30e200bed6962522d2dae670e5fe7e0330b2f6f7
tonmoy <tonmoy3673@gmail.com>
2023-09-10 23:25:28 +0600

    create loadPhone function

--- apps.js
+++ apps.js
@@ -8,3 +8,11 @@
 
 // Example: https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089
 
+// ========= loadPhone ================//
+const loadPhone=async()=>{
+    const url='https://openapi.programming-hero.com/api/phones?search=iphone';
+    const res= await fetch(url);
+    const data=await res.json();
+    console.log(data.data);
+};
+loadPhone();
