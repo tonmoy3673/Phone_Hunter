1a84759564fb6eab66f22402bb3bcd9c81ce746a
tonmoy <tonmoy3673@gmail.com>
2023-09-12 00:50:08 +0600

    set info

--- apps.js
+++ apps.js
@@ -128,11 +128,17 @@ phoneDetails();
 
 const phoneModal=(phone)=>{
     console.log(phone)
-    const {name,image,brand}=phone
+    const {name,image,brand,mainFeatures,releaseDate}=phone
     const modalTitle=document.getElementById('exampleModalLabel');
     modalTitle.innerHTML=`${name}`
     const modalDetails=document.getElementById('modalBody');
     modalDetails.innerHTML=`
+    <img src=${image} class="card-img-top w-75 mx-auto py-2" alt="phoneImage">
+      <div class="card-body">
+        <h4 class="card-title text-info fw-bolder">${brand}</h4>
+        <h5 class="card-text ">Memory : ${mainFeatures.memory}</h5>
+        <h6 class="card-text text-secondary">${releaseDate}</h6>
+      </div>
     
     `
 }
