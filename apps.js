f5ac98c423962493d66236bb8de7bbd305b6e10b
tonmoy <tonmoy3673@gmail.com>
2023-10-17 02:21:39 +0600

    display phone modal

--- apps.js
+++ apps.js
@@ -43,7 +43,6 @@ const displayPhones=(phones,limit)=>{
 
     phoneContainer.innerText=' ';
     phones.forEach(phone => {
-        console.log(phone)
         const {brand,image,phone_name,slug}=phone;
        const oneDiv=document.createElement('div');
        oneDiv.classList.add('col');
@@ -98,7 +97,25 @@ showPhoneModal(data.data)
 // ========= show phone modal ==========//
 
 const showPhoneModal=(phone)=>{
+    const {brand,image,mainFeatures,name,others,releaseDate}=phone;
+const modalTitle=document.getElementById('exampleModalLabel');
+modalTitle.innerHTML=`<h5 class="text-center text-info">${name}</h5>`
+const modalBody=document.getElementById('modal-body');
+modalBody.innerHTML=`
 
+<div class="card">
+            <img src=${image} class="card-img-top img-fluid w-50 mx-auto py-2 mt-2 rounded-3" alt="phone">
+            <div class="card-body">
+              <h5 class="card-title text-center text-info py-2">${brand}</h5>
+              <h6 class='text-center'>Display : ${mainFeatures?.displaySize?mainFeatures?.displaySize.slice(0,22):'Not Found'}</h6>
+              <h6 class='text-center'>Memory : ${mainFeatures?.memory?mainFeatures?.memory.slice(0,20):'Not Found'}</h6>
+              <h6 class='text-center'>Bluetooth : ${others?.Bluetooth?others?.Bluetooth:'Not Found'}</h6>
+              <h6 class='text-center text-secondary'> ${releaseDate?releaseDate:'Not Found'}</h6>
+              
+            </div>
+          </div>
+
+`
 console.log(phone)
 }
 
