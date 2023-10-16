a5047a227f4c9cba7b6c4516fd7b0f1e445e0a33
tonmoy <tonmoy3673@gmail.com>
2023-10-16 17:12:44 +0600

    implementted phone card

--- apps.js
+++ apps.js
@@ -8,4 +8,38 @@
 
 // Example: https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089
 
-console.log('connected')
+
+// ============ loadPhone API ===========//
+const loadPhones=async()=>{
+    const url='https://openapi.programming-hero.com/api/phones?search=oppo';
+    const res=await fetch(url);
+    const data=await res.json();
+    displayPhones(data.data)
+};
+
+
+// =========== displayPhone function ===========//
+const displayPhones=(phones)=>{
+    const phoneContainer=document.getElementById('phone-container');
+    phones.forEach(phone => {
+        console.log(phone)
+        const {brand,image,phone_name,slug}=phone;
+       const oneDiv=document.createElement('div');
+       oneDiv.classList.add('col');
+       oneDiv.innerHTML=`
+       <div class="card h-100">
+            <img src=${image} class="card-img-top img-fluid w-75 mx-auto py-3 rounded-3" alt="phone">
+            <div class="card-body">
+              <h5 class="card-title text-center text-info py-2">${brand}</h5>
+              <h6 class='text-center'>Device : ${phone_name}</h6>
+              <h6 class='text-center'>Model : ${slug.length>17? slug.slice(0,17):slug}</h6>
+            </div>
+          </div>
+       `
+
+       phoneContainer.appendChild(oneDiv);
+    });
+   
+}
+
+loadPhones();
